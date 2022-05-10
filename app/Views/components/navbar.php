d9532888f91b1cda0c99e2db09cba740cd30a7e5
richard-conectsen <87664755+richard-conectsen@users.noreply.github.com>
2022-05-09 22:06:16 -0500

    Add files via upload

---
+++ app/Views/components/navbar.php
@@ -0,0 +1,90 @@
+<!-- NAVIGATION RAPIMONEY -->
+<nav class="mi-navbar navbar is-fixed-top" role="navigation" aria-label="main navigation">
+
+  <div class="navbar-brand">
+
+    <a class="navbar-item" href="<?php echo base_url() ?>">
+      <img src="<?php echo base_url('/assets/img/header/logo-rapimoney.png') ?>"
+        alt="rapimoney:retira efectivo con tu tarjeta de crédito">
+    </a>
+
+    <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+      <span class="icon-burger" aria-hidden="true"></span>
+      <span class="icon-burger" aria-hidden="true"></span>
+      <span class="icon-burger" aria-hidden="true"></span>
+    </a>
+
+  </div>
+
+  <div id="navMenu" class="navbar-menu">
+    <div class="navbar-end">
+
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link navbar-item--style">
+          SERVICIOS
+        </a>
+
+        <div class="navbar-dropdown">
+
+          <a class="navbar-item" href="<?php echo base_url(); ?>/retiro-efectivo" rel="noopener noreferrer">
+            <p class="navbar-item--substyle">RETIRA EFECTIVO CON TARJETA</p>
+          </a>
+
+          <a class="navbar-item" href="<?php echo base_url(); ?>/evaluacion" rel="noopener noreferrer">
+            <p class="navbar-item--substyle">PRÉSTAMO CON EVALUACIÓN</p>
+          </a>
+
+          <a class="navbar-item navbar-item--substyle" href="<?php echo base_url(); ?>/garantia"
+            rel="noopener noreferrer">
+            <p class="navbar-item--substyle">PRÉSTAMO CON GARANTÍA</p>
+          </a>
+
+          <a class="navbar-item navbar-item--substyle" href="<?php echo base_url(); ?>/pago-servicios"
+            rel="noopener noreferrer">
+            <p class="navbar-item--substyle">PAGO DE RECIBO CON DSCTO</p>
+          </a>
+
+        </div>
+      </div>
+
+      <!-- <a class="navbar-item navbar-item--style" href="<?php echo base_url(); ?>/#invest">
+        <p class="">QUIERO INVERTIR</p>
+      </a> -->
+
+      <!-- <a class="navbar-item navbar-item--style" href="<?php echo base_url(); ?>/#working">
+        <p class="">TRABAJA CON NOSOTROS</p>
+      </a> -->
+
+      <a class="navbar-item navbar-item--style" href="<?php echo base_url(); ?>/pago-servicios"
+        rel="noopener noreferrer">
+        <p>AHORRA AQUÍ</p>
+      </a>
+
+      <a class="navbar-item navbar-item--style" href="https://rapimoney.blogspot.com/" target="_blank"
+        rel="noopener noreferrer">
+        <p>BLOG</p>
+      </a>
+
+      <a class="navbar-item navbar-item--style" href="<?php echo base_url(); ?>/nosotros">
+        <p>NOSOTROS</p>
+      </a>
+
+
+      <!--
+      <a class=" navbar-item navbar-item--style" href="<?php #echo base_url(); ?>/contacto">
+        <p>CONTACTO</p>
+      </a> -->
+
+      <!-- <a class=" navbar-item navbar-item--style" href="<?php #echo base_url(); ?>">
+        <p>INICIO</p>
+      </a> -->
+
+
+      <!-- <a class="navbar-item navbar-item__button" href="<?php #echo base_url(); ?>">
+        <button class="button is-success is-rounded is-size-6 navbar-item__button--style">INICIO</button>
+      </a> -->
+
+    </div>
+
+</nav>
